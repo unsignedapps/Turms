980f9baa2f7b406a80d5bc9d09b5371c91c43629
Rob Amos <bok@unsignedapps.com>
2015-11-06 15:56:25 +1100

    Initial Commit

---
+++ Turms/Turms.h
@@ -0,0 +1,19 @@
+//
+//  Turms.h
+//  Turms
+//
+//  Created by Robert Amos on 6/11/2015.
+//  Copyright © 2015 Unsigned Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Turms.
+FOUNDATION_EXPORT double TurmsVersionNumber;
+
+//! Project version string for Turms.
+FOUNDATION_EXPORT const unsigned char TurmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Turms/PublicHeader.h>
+
+
---
+++ TurmsTests/TurmsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TurmsTests.swift
+//  TurmsTests
+//
+//  Created by Robert Amos on 6/11/2015.
+//  Copyright © 2015 Unsigned Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Turms
+
+class TurmsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
